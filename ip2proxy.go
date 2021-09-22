e0c06994253221c368d257b9230ce407918d3ccb
IP2Location <support@ip2location.com>
2021-09-22 16:13:05 +0800

    Remove else block

--- ip2proxy.go
+++ ip2proxy.go
@@ -967,12 +967,12 @@ func (d *DB) query(ipAddress string, mode uint32) (ip2proxyRecord, error) {
 			}
 
 			return x, nil
+		}
+
+		if ipNo.Cmp(ipFrom) < 0 {
+			high = mid - 1
 		} else {
-			if ipNo.Cmp(ipFrom) < 0 {
-				high = mid - 1
-			} else {
-				low = mid + 1
-			}
+			low = mid + 1
 		}
 	}
 	return x, nil
